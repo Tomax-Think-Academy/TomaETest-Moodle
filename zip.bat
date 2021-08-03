e5ddc3f500ad5a4323462835033911ff977adbd7
tzahiba <37371152+tzahiba@users.noreply.github.com>
2021-08-03 15:02:49 +0300

    add zip.bat

---
+++ zip.bat
@@ -0,0 +1,7 @@
+cd ./accessrule
+tar.exe -a -c -f TET-accessrule-moodle.zip tomaetest
+move TET-accessrule-moodle.zip ../
+cd ../report/
+tar.exe -a -c -f TET-report-moodle.zip tomaetest
+move TET-report-moodle.zip ../
+pause
